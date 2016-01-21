788769aad69238342cccfed02866ce9e4567fd80
Jason Williams <jasonmw_nf@hotmail.com>
2016-01-21 01:52:20 -0330

    quick update

--- Calculator/ViewController.m
+++ Calculator/ViewController.m
@@ -25,11 +25,48 @@
 }
 
 - (IBAction)buttonDigit:(id)sender {
+    
+    currentNumber = currentNumber *10 + (float)[sender tag];
+    self.Label.text = [NSString stringWithFormat:@"%2.0f", currentNumber];
+    
 }
 
 - (IBAction)buttonOperation:(id)sender {
+    
+    if (currentOperation == 0) result = currentNumber;
+    else {
+        switch (currentOperation) {
+            case 1:
+                result = result + currentNumber;
+                break;
+            case 2:
+                result = result - currentNumber;
+                break;
+            case 3:
+                result = result * currentNumber;
+                break;
+            case 4:
+                result = result / currentNumber;
+                break;
+                
+            default:
+                break;
+        }
+    }
+    
+    currentNumber = 0;
+    self.Label.text = [NSString stringWithFormat:@"%2.0f", result];
+    if ([sender tag] == 0) result = 0;
+    {
+        currentOperation = (int)[sender tag];
+    }
+    
 }
 
 - (IBAction)Cancel:(id)sender {
+    
+    currentNumber = 0;
+    self.Label.text = @"0";
+    
 }
 @end
